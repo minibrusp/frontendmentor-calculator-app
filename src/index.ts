2668ba08a62ffa6680667e936c507b28ec947383
XIII <minibrusp@gmail.com>
2023-06-11 05:22:46 +0800

    Initial setup for vanilla typescript and sass

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { logger } from "./xample/xample"
+import "./styles/index.scss"
+
+console.log("******************************* loheeee! ! !")
+console.log("Hello")
+console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+
+logger()
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, "src")]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  }
+}
