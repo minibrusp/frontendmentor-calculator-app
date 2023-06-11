9e35e89efcc0354cfb2f7a4e02af7fed209b9e8a
XIII <minibrusp@gmail.com>
2023-06-12 07:24:30 +0800

    - Added Mobile Design 375px - Added Theme Switch Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-import { logger } from "./xample/xample"
 import "./styles/index.scss"
 
-console.log("******************************* loheeee! ! !")
-console.log("Hello")
-console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
 
-logger()
+const form = document.querySelector("form") as HTMLFormElement
+
+const switchThemeClass = (value: number ): void => {
+  console.log(value)
+  
+  switch(value) {
+    case 1:
+      document.body.classList.value = ""
+      break
+    case 2: 
+      document.body.classList.value = 'second-theme'
+      break
+    case 3:
+      document.body.classList.value = 'third-theme'
+      break
+    default: 
+      document.body.classList.value = ""
+      break
+  }
+}
+
+
+form.addEventListener('change', (e: Event) => {
+  e.preventDefault()
+
+  let checkedElement = document.querySelector('input[name="theme"]:checked') as HTMLInputElement
+  checkedElement.checked = true
+  switchThemeClass(Number(checkedElement.value))
+  
+})
+
+
+
