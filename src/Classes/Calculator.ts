2ccab81c0b65f8faa1ad469812de64b075d43a7d
XIII <minibrusp@gmail.com>
2023-06-14 03:15:17 +0800

    - Added Class method for event dotButton click - Added Class method for event switch theme - refactor theme switch function into the class method

--- src/Classes/Calculator.ts
+++ src/Classes/Calculator.ts
@@ -1,21 +1,65 @@
 export class Calculator {
-  // private screen: HTMLInputElement | undefined
   constructor(
-    // public result: string,
-    // public num1: number,
-    // public num2: number,
-    // public operator: string,
-    private screen: HTMLInputElement
+    private screen: HTMLInputElement,
+    private theme: HTMLBodyElement,
   ){}
 
-  setScreen(screen: HTMLInputElement) {
-    this.screen = screen
+  switchTheme(value: number) {
+    switch(value) {
+      case 1:
+        this.theme.classList.value = ""
+        break
+      case 2: 
+        this.theme.classList.value = 'second-theme'
+        break
+      case 3:
+        this.theme.classList.value = 'third-theme'
+        break
+      default: 
+        this.theme.classList.value = ""
+        break
+    }
+  }
+
+  addDot() {
+    
+    if(
+        !Number(this.screen.value.slice(-1)) 
+        && this.screen.value.slice(-1) !== "." 
+        && this.screen.value.slice(-1) !== "0"
+      ) {
+      return
+    }
+
+    if(!this.screen.value.includes(".")) {
+      return this.disPlayButtonToScreen(".")
+    }
+    
+    if(
+        this.screen.value.includes(".") 
+        && !Number(this.screen.value.slice(-1))
+        && this.screen.value.slice(-1) !== "0"
+        && this.screen.value.slice(-1) !== "."
+        ||
+        this.screen.value.includes(".")
+        && Number(this.screen.value.slice(-1))
+        && this.screen.value.slice(-1) !== "0"
+        && this.screen.value.slice(-1) !== "."
+        ||
+        this.screen.value.includes(".")
+        && !Number(this.screen.value.slice(-1))
+        && this.screen.value.slice(-1) === "0"
+        && this.screen.value.slice(-1) !== "."
+      ) return
+
+    this.screen.value = this.screen.value.replace(".", "")
+    
   }
 
   disPlayButtonToScreen(value: string) {
     let newValue = value.replace(/X/g, '*')
     newValue = newValue.replace(",", "")
-    if(Number(newValue)) {
+    if(Number(newValue) || newValue === "0" || newValue === ".") {
       return this.screen.value += value
     }
     return this.screen.value += ` ${value} `
@@ -23,20 +67,16 @@ export class Calculator {
 
   delete() {
     this.screen!.value = this.screen?.value.slice(0, -1)    
-    console.log(this.screen!.value);
   }
   reset() {
     this.screen!.value = ""
   }
 
   calculate() {
-    console.log(this.screen!.value)
     let newValue = this.screen.value.replace(/X/g, '*')
-      newValue = newValue.replace(",", "")
-    // return eval(text)
+    newValue = newValue.replace(/[,\s]/,"").replace(/[,\s]/,"").replace(/[,\s]/,"").replace(/[,\s]/,"")
     let result = eval(newValue)
-    console.log(eval(result))
     
-    this.screen.value = eval(result)
+    this.screen.value = result
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,58 +3,27 @@ import { Calculator } from "./Classes/Calculator"
 
 
 const form = document.querySelector("form") as HTMLFormElement
-
-const switchThemeClass = (value: number ): void => {
-  console.log(value)
-  
-  switch(value) {
-    case 1:
-      document.body.classList.value = ""
-      break
-    case 2: 
-      document.body.classList.value = 'second-theme'
-      break
-    case 3:
-      document.body.classList.value = 'third-theme'
-      break
-    default: 
-      document.body.classList.value = ""
-      break
-  }
-}
-
-
-form.addEventListener('change', (e: Event) => {
-  e.preventDefault()
-
-  let checkedElement = document.querySelector('input[name="theme"]:checked') as HTMLInputElement
-  checkedElement.checked = true
-  switchThemeClass(Number(checkedElement.value))
-})
-
+const body = document.body as HTMLBodyElement
 const calculateBtn = document.querySelector("button.calculate") as HTMLButtonElement
 const deleteBtn = document.querySelector("button.delete") as HTMLButtonElement
 const resetBtn = document.querySelector("button.reset") as HTMLButtonElement
+const dotBtn = document.querySelector("button.dotBtn") as HTMLButtonElement
 const normalBtns = document.querySelectorAll("button.normalBtn") as NodeListOf<HTMLButtonElement>
 let screen = document.querySelector("input#output") as HTMLInputElement
 
-// const MyCalculator = new Calculator()
 
-// calculateBtn.addEventListener("click", (e: Event) => {
-//   let value = document.querySelector("input#output") as HTMLInputElement
-//   let result = value.value.replace(/X/g, '*');
-//   result = result.replace(",", "")
-//   let resultArray = result.split(" ")
-//   let changeToNumber = (Number(resultArray[0]))
+const MyCalculator = new Calculator(screen, body)
 
-//   if(Number.isNaN(changeToNumber)) return
-  
-//   result = MyCalculator.calculate(result)
-//   value.value = Number(result).toLocaleString("en-US")
-// })
+form.addEventListener('change', (e: Event) => {
+  e.preventDefault()
+  let checkedElement = document.querySelector('input[name="theme"]:checked') as HTMLInputElement
+  checkedElement.checked = true
+  MyCalculator.switchTheme(Number(checkedElement.value))
+})
 
-
-const MyCalculator = new Calculator(screen)
+dotBtn.addEventListener("click", () => {
+  MyCalculator.addDot()
+})
 
 calculateBtn.addEventListener("click", () => {
   if(screen.value === "") return
@@ -71,7 +40,6 @@ resetBtn.addEventListener("click", () => {
 
 normalBtns.forEach((normalBtn: HTMLButtonElement) => {
   normalBtn.addEventListener("click", (e: Event) => {
-    console.log(normalBtn.value)
     
     MyCalculator.disPlayButtonToScreen(normalBtn.value)
   })
